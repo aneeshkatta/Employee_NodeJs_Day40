1571e813c089923a47edfb5f6aaf09b7d429a0ae
Aneesh <katta.aneesh597@gmail.com>
2022-12-30 12:55:23 +0530

    added program for employee wage using function

--- EmployeeWageUc.js
+++ EmployeeWageUc.js
@@ -6,19 +6,20 @@ const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
 let empHrs = 0;
 let empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case  IS_FULL_TIME :
-        console.log("Employee is Full Time");
-        empHrs = 8;
-        break;
-    case  IS_PART_TIME :
-        console.log("Employee is Part Time");
-        empHrs = 8;
-        break;
-    default :
-    console.log("Employee is Absent");
-        empHrs = 0;
-        break;
+function GetEmpHrs(empCheck) {
+    switch (empCheck) {
+        case IS_FULL_TIME:
+            return FULL_TIME_HRS;
+            break;
+        case IS_PART_TIME:
+            return PART_TIME_HRS;
+            break;
+        default:
+            return IS_ABSENT;
+            break;
+    }
+
 }
-let empWage=empHrs*WAGE_PER_HR;
-console.log("Employee Wage:"+empWage);
+empHrs=GetEmpHrs(empCheck);
+let empWage = empHrs * WAGE_PER_HR;
+console.log("Employee Wage:" + empWage);
