7a27114531feb17f6c8f8c0f9c611a486adafb34
Aneesh <katta.aneesh597@gmail.com>
2022-12-30 15:30:26 +0530

    added program for first occurance of full time wage

--- EmployeeWageUc.js
+++ EmployeeWageUc.js
@@ -6,7 +6,6 @@ const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
 const NO_OF_WORKING_DAYS = 20;
 let empDailyWagearr = new Array();
-//Uc_7c Show Days when Full time wage of 160 were earned using filter function
 function GetEmpHrs(empCheck) {
     switch (empCheck) {
         case IS_FULL_TIME:
@@ -51,14 +50,12 @@ console.log("UC_7B :Total Working Days: " + totalWorkingDays + " Total Hrs: " + 
 let dailycounter=0;
 function mapDaywithWage(dailywage) {
     dailycounter++;
-    return dailycounter+"="+dailywage;
+    return dailycounter+"="+dailywage+"\t";
 }
 let aMapDaywithWage =empDailyWagearr.map(mapDaywithWage)
 console.log("UC_7B: DailyWageMap:\n"+aMapDaywithWage);
-//filter function
-function FullTimeWage(dailywage) {
+//uc 7D_to Find the first occurrence when Full Time Wage was earned using find function;
+function FindFulltimeWage(dailywage) {
     return dailywage.includes("160");
 }
-let aFullTimeWage=aMapDaywithWage.filter(FullTimeWage);
-console.log("Uc_7c Show Days when Full time wage of 160 were earned using filter function")
-console.log(aFullTimeWage);
+console.log("uc 7D_to Find the first occurrence when Full Time Wage was earned using find function i.e firstoccurance:160=\t"+aMapDaywithWage.find(FindFulltimeWage));
