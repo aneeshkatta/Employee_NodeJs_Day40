756d2d97e8ece8d11254327b0fc74eb7ab1ee527
Aneesh <katta.aneesh597@gmail.com>
2022-12-30 13:54:23 +0530

    added program for employee wage  store in array

--- EmployeeWageUc.js
+++ EmployeeWageUc.js
@@ -5,6 +5,8 @@ const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
 const NO_OF_WORKING_DAYS = 20;
+let empDailyWagearr = new Array();
+//uc6-Store the Daily Wage along with the Total Wage - Save in an Array the Daily Wage
 function GetEmpHrs(empCheck) {
     switch (empCheck) {
         case IS_FULL_TIME:
@@ -19,14 +21,23 @@ function GetEmpHrs(empCheck) {
     }
 
 }
+function CalculateDailyWage(empHrs, WAGE_PER_HR) {
+    return empWagePerMonth = empHrs * WAGE_PER_HR;
+}
 let totalWorkingDays = 0;
-let totalEmpHrs=0;
-const MAX_HRS_IN_MONTH=100;
-while ( totalWorkingDays < NO_OF_WORKING_DAYS && totalEmpHrs<MAX_HRS_IN_MONTH ) {
+let totalEmpHrs = 0;
+const MAX_HRS_IN_MONTH = 100;
+while (totalWorkingDays < NO_OF_WORKING_DAYS && totalEmpHrs < MAX_HRS_IN_MONTH) {
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += GetEmpHrs(empCheck);   
+    empHrs = GetEmpHrs(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWagearr.push(CalculateDailyWage(empHrs,WAGE_PER_HR));
     totalWorkingDays++;
 }
-let empWagePerMonth = totalEmpHrs * WAGE_PER_HR;
-    console.log("Total Working Days: "+totalWorkingDays+" Total Hrs: " + totalEmpHrs + " Employee Wage:" + empWagePerMonth);
-
+let NetMonthlyWage = CalculateDailyWage(totalEmpHrs, WAGE_PER_HR);
+console.log("Total Working Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Employee Wage:" + NetMonthlyWage);
+console.log("printing stored daily wage in array:");
+for (i = 0; i < empDailyWagearr.length; i++)
+{
+console.log((i+1) + ":index " + empDailyWagearr[i]);
+}
