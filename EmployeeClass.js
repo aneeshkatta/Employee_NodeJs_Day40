856a07337f026e32b5e14c506d71535ae5cd8891
Aneesh <katta.aneesh597@gmail.com>
2023-01-08 12:46:06 +0530

    added program

---
+++ EmployeeClass.js
@@ -0,0 +1,22 @@
+class EmployeePayrollData{
+    id;
+    salary;
+    constructor(id,name,salary)
+    {
+        this.id=id;
+        this.name=name;
+        this.salary=salary;
+    }
+    get name()  {return this._name;}
+    set name(name)  {this._name=name;}
+    toString()
+    {
+        return "id="+this.id+", name="+this.name+", salary="+this.salary;
+    }
+}
+
+let employeePayrollData=new EmployeePayrollData(1,"ravi",50000);
+console.log(employeePayrollData);
+employeePayrollData.name="ram";
+employeePayrollData.salary=500000;
+console.log(employeePayrollData);
