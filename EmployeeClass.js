2328b1f05aac88bbc98a3a2aa15038904f4d531b
Aneesh <katta.aneesh597@gmail.com>
2023-01-08 13:06:22 +0530

    added program

--- EmployeeClass.js
+++ EmployeeClass.js
@@ -1,22 +1,28 @@
 class EmployeePayrollData{
     id;
     salary;
-    constructor(id,name,salary)
-    {
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
+    gender;
+    startDate;
+    constructor(...params){
+        this.id=params[0];
+        this.name=params[1];
+        this.salary=params[2];
+        this.gender=params[3];
+        this.startDate=params[4];
     }
     get name()  {return this._name;}
     set name(name)  {this._name=name;}
+    get salary()  {return this._salary;}
+    set salary(salary)  {this._salary=salary;}
+    get gender()  {return this._gender;}
+    set gender(gender)  {this._gender=gender;}
+    get startDate()  {return this._startDate;}
+    set startDate(startDate)  {this._startDate=startDate;}
     toString()
     {
-        return "id="+this.id+", name="+this.name+", salary="+this.salary;
+        return "id="+this.id+", name="+this.name+", salary="+this.salary+", gender="+this.gender+", startDate="+this.startDate;;
     }
 }
 
-let employeePayrollData=new EmployeePayrollData(1,"ravi",50000);
-console.log(employeePayrollData.toString());
-employeePayrollData.name="ram";
-employeePayrollData.salary=500000;
+let employeePayrollData=new EmployeePayrollData(1,"ravi",50000,"M",05-05-2022);
 console.log(employeePayrollData.toString());
